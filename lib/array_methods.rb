03104ed208cffb5c4c4c501e9a9f43f31d7a7d61
Renée Cruz <reneebasscruz@gmail.com>
2019-07-22 03:23:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,3 +9,36 @@ end
 def find_min_value(array)
   array.min
 end
+
+def find_element_index(array, value_to_find)
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    end
+  end
+  nil
+end
+
+def find_max_value(array)
+  max = 0
+    array.length.times do |count|
+      if array[count] > max
+        max = array[count]
+      end
+    end
+  max
+end
+
+def find_min_value(array)
+  min = 0
+    array.length.times do |count|
+      if count == 0
+        min = array[count]
+      else
+        if array[count] < min
+          min = array[count]
+        end
+      end
+    end
+  min
+end
